39eaa3973a11bd7ec44ace73a55cbeb20a7bf37e
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-02-18 16:12:44 -0600

    Instalacion de paquetes

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// Importamos express
+const express = require("express");
+const app = express();
+
+// Aplicacion a la escucha
+app.listen(8800, () => {
+  console.log("El Backend esta jalando");
+});
