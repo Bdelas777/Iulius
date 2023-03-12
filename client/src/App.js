62b2ffef2699b51f0068e6a19b23a28a26614259
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-03-12 16:45:34 -0600

    topbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div>Hello Iulius</div>;
+  return <Home />;
 }
 
 export default App;
---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./topbar.css";
+
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo"> Iulius</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Busca amigos o publica un video o una foto"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Inicio</span>
+          <span className="topbarLink">Perfil</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarBadge">1</span>
+          </div>
+        </div>
+        <img
+          src="/assets/users/1.jpeg"
+          alt="Ejemplo de foto"
+          className="topbarImg"
+        />
+      </div>
+    </div>
+  );
+}
